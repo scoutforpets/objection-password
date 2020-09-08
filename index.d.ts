66d9809f8db8c4d711372740501a2feaa8a7f567
Benjamin Marquis <44323490+BjMrq@users.noreply.github.com>
2020-09-07 22:35:02 -0400

    Update index.d.ts
    Hello, the types were not working properly this fixes the issue (issue 29) in my project, I am fairly new to Typescript so any review would be appreciated!

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,18 @@
-import { Model, Page, QueryContext } from 'objection';
-
 declare module 'objection-password' {
-  class PasswordQueryBuilder<M extends Model, R = M[]> {
-    ArrayQueryBuilderType: PasswordQueryBuilder<M, M[]>;
-    SingleQueryBuilderType: PasswordQueryBuilder<M, M>;
-    NumberQueryBuilderType: PasswordQueryBuilder<M, number>;
-    PageQueryBuilderType: PasswordQueryBuilder<M, Page<M>>;
-  }
+  import { Model } from 'objection';
 
-  interface PasswordInstance<T extends typeof Model> {
-    QueryBuilderType: PasswordQueryBuilder<this & T['prototype']>;
+  interface PasswordInstance extends Model {
 
-    $beforeInsert(context: QueryContext): Promise<string | void>;
-    $beforeUpdate(queryOptions: object, context: QueryContext): Promise<string | void | undefined>
     verifyPassword(password: string): Promise<boolean>;
     generateHash(): Promise<string | void>;
+
   }
 
-  interface PasswordStatic<T extends typeof Model> {
-    QueryBuilder: typeof PasswordQueryBuilder;
-
-    isBcryptHash(str: string): boolean;
-
-    new(): PasswordInstance<T> & T['prototype'];
-  }
+  type Constructor<T = {}> = new (...args: any[]) => T;
 
   export default function (options?: {
     allowEmptyPassword?: boolean;
     passwordField?: string;
     rounds?: number;
-  }): <T extends typeof Model>(model: T) => PasswordStatic<T> & Omit<T, 'new'> & T['prototype'];
+  }): <T extends typeof Model>(model: T) => T & Constructor<PasswordInstance>
 }
